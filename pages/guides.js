c8d9d71e47b62b10b06fa0ac3fcffa7a9d6a5a4f
clear <harry.hareesha@adventhealth.com>
2021-10-03 10:41:43 -0700

    init

---
+++ pages/guides.js
@@ -0,0 +1,11 @@
+import { useContext, useEffect, useState } from 'react'
+import styles from '../styles/Guides.module.css'
+
+
+export default function Guides() {
+ return (
+   <div className={styles.guides}>
+     guide
+   </div>
+ )
+}
